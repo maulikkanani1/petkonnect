622648ed36433b369d332d511902b67ef21bb50f
Akash Chaudhary <akashfzd2000@gmail.com>
2021-05-08 20:39:51 +0530

    added pagination

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,59 @@ import { Nav } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
 
 const Products = ({ stores }) => {
+
+  const [state,setState] = useState({currentPage: 1,itemsPerPage: 6});
+
+  const {currentPage, itemsPerPage} = state;
+
+  const lastIndex = currentPage * itemsPerPage;
+  const firstIndex = lastIndex - itemsPerPage;
+  const currentItems = stores.slice(firstIndex,lastIndex);
+
+  const pgNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(stores.length / itemsPerPage); i++) {
+    pgNumbers.push(i);
+  }
+
+  function handleClick(e) {
+    setState(state => {
+      return {
+        ...state,
+        currentPage: Number(e.target.id)
+      }
+    })
+  }
+
+  function handleNext() {
+    setState(state => {
+      return {
+        ...state,
+        currentPage: currentPage == lastIndex  ? 1 : currentPage+1
+      }
+    })
+  }
+
+  function handlePrev() {
+    setState(state => {
+      return {
+        ...state,
+        currentPage: currentPage  == 1 ? lastIndex : currentPage-1
+      }
+    })
+  }
+
+  const displayPageNumbers = pgNumbers.map(pgnum => {
+    return (
+      <li className = "page-link"  key = {pgnum} id={pgnum} onClick = {handleClick}>
+        {pgnum}
+      </li>
+    );
+  });
+
+  console.log('hey items:',currentItems.length);
+  console.log('hey products:',stores.length);
+
   return (
     <div className="products_container">
       <h5 className="text-primary text-center mt-5">Top Products</h5>
@@ -19,10 +72,18 @@ const Products = ({ stores }) => {
         </Nav.Item>
       </Nav>
       <div className="row mt-5 mb-5">
-        {stores.map((store, i) => (
+        
+        {currentItems.map((store, i) => (
           <ProductCard product={store} vendor={store["vendorID"]} id={store["vendorID"].id} />
         ))}
       </div>
+      <nav aria-label="Page navigation example justify-content-center">
+        <ul className ="pagination">
+          <li className="page-link" >Prev</li>
+          {displayPageNumbers}
+          <li className="page-link" onClick={handleNext}>Next</li>
+        </ul>
+    </nav>
     </div>
   );
 };
